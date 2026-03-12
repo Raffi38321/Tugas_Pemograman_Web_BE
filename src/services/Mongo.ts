bd3e764da81731992ab54318becccd0451b03af1
Raffi <raffi38321@gmail.com>
2026-03-12 19:19:51 +0700

    hmm

--- src/services/Mongo.ts
+++ src/services/Mongo.ts
@@ -1,13 +1,20 @@
 import mongoose from "mongoose";
 import ENV from "../utils/ENV";
 
+let isConnected = false;
+
 const connect = async () => {
+  if (isConnected) {
+    return;
+  }
+
   try {
-    await mongoose.connect(ENV.MONGO_URL);
+    const db = await mongoose.connect(ENV.MONGO_URL);
+
+    isConnected = db.connections[0].readyState === 1;
     console.log("berhasil konek db");
   } catch (error) {
     console.log(error);
-    process.exit(1);
   }
 };
 
